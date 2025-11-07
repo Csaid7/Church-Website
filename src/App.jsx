198197a8a8770d9895c85729078a9166afab999c
Csaid7 <mpungu2005@gmail.com>
2025-11-07 12:28:41 -0500

    Set up react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1> Success Point Church </h1>
+      <h2> Happy to see you</h2>
+      <p> Welcome to the Success Point Church's Website! </p>
+    
+    </div>
+  );
+}
+export  default App;
